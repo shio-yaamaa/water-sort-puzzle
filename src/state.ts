7686ec04aaa1eee562690c06190dbeb0228705df
shio-yaamaa <shio.yama520@gmail.com>
2021-03-26 12:01:17 +0900

    Implement state-related functions

---
+++ src/state.ts
@@ -0,0 +1,58 @@
+import { Tube, State } from "./types";
+import { stringifyTube, isTubeComplete, pourMaximum } from "./tube";
+
+// Convert State.tubes into a string.
+// Only the same combinations of tubes (not considering the order) convert to the same string.
+export const stringifyTubes = (tubes: Tube[]): string => {
+  return tubes
+    .map((tube) => stringifyTube(tube))
+    .sort()
+    .join(".");
+};
+
+export const isStateComplete = (state: State) => {
+  return state.tubes.every((tube) => isTubeComplete(tube));
+};
+
+const isStateRepeating = (state: State, newTubes: Tube[]): boolean => {
+  return state.history.has(stringifyTubes(newTubes));
+};
+
+export const yieldChildStates = (state: State): State[] => {
+  if (isStateComplete(state)) {
+    return [];
+  }
+  const childStates: State[] = [];
+  state.tubes.forEach((sourceTube, sourceIndex) => {
+    state.tubes.forEach((destinationTube, destinationIndex) => {
+      if (sourceIndex === destinationIndex) {
+        return;
+      }
+      const [
+        updatedSourceTube,
+        updatedDestinationTube,
+        isSuccessful,
+      ] = pourMaximum(sourceTube, destinationTube);
+      if (!isSuccessful) {
+        return;
+      }
+      const childTubes = state.tubes.map((tube, index) => {
+        if (index === sourceIndex) {
+          return updatedSourceTube;
+        }
+        if (index === destinationIndex) {
+          return updatedDestinationTube;
+        }
+        return tube;
+      });
+      if (isStateRepeating(state, childTubes)) {
+        return;
+      }
+      childStates.push({
+        tubes: childTubes,
+        history: new Set(state.history).add(stringifyTubes(state.tubes)),
+      });
+    });
+  });
+  return childStates;
+};
