0b6dc4969ddeecc82635c5e30847f0a868470355
shio-yaamaa <shio.yama520@gmail.com>
2021-03-28 03:23:27 +0900

    Fix import

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,3 +1,3 @@
 export { Stage, State, Tube, Water } from "./types";
-export { stages } from "../../data/stages";
+export { stages } from "../data/stages";
 export { stringifyTubes, isStateComplete, yieldChildStates } from "./state";
