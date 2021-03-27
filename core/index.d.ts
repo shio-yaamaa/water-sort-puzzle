bdfe78f49130ab1510d74182a922203782fdb3d9
shio-yaamaa <shio.yama520@gmail.com>
2021-03-28 04:36:32 +0900

    Remove type declaration file

--- core/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Stage, State, Tube } from "./src/types";
-export { Stage, State, Water, Tube } from "./src/types";
-
-export const stages: Stage[];
-export const stringifyTubes: (tubes: Tube[]) => string;
-export const isStateComplete: (state: State) => boolean;
-export const yieldChildStates: (state: State) => State[];
