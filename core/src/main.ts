4d3e6ea264d224c62c60a6d057ee3c44ec31af9f
shio-yaamaa <shio.yama520@gmail.com>
2021-03-29 04:34:00 +0900

    Export videos

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,3 +1,4 @@
 export { Stage, State, Tube, Water } from "./types";
 export { stages } from "../data/stages";
+export { videos } from "../data/videos";
 export { stringifyTubes, isStateComplete, yieldChildStates } from "./state";
