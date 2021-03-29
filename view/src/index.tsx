9d065bd0ab96a14c13330d5565d3f44801c579fb
shio-yaamaa <shio.yama520@gmail.com>
2021-03-30 08:22:31 +0900

    Fix imports

--- view/src/index.tsx
+++ view/src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App } from "./components/App";
+import { BrutoForcePage } from "./pages/BrutoForcePage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrutoForcePage />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- view/src/pages/BrutoForcePage.tsx
+++ view/src/pages/BrutoForcePage.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import "./App.css";
-import { Row } from "./Row";
+import "./BrutoForcePage.css";
+import { Row } from "../components/Row";
 import { stages } from "core";
 import { solve } from "../solve";
 
 const transfers = solve(stages[3]);
 
-export const App: React.VFC = () => {
+export const BrutoForcePage: React.VFC = () => {
   return (
-    <div className="App">
+    <div className="BrutoForcePage">
       {transfers.map((transfer, index) => (
         <Row
           key={index}
