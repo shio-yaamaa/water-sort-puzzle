357e0d469604a8bc9db32246d7defc04640a0cb0
shio-yaamaa <shio.yama520@gmail.com>
2021-03-30 08:27:04 +0900

    Add DifficultyTablePage

--- view/src/index.tsx
+++ view/src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrutoForcePage } from "./pages/BrutoForcePage";
+import { DifficultyTablePage } from "./pages/DifficultyTablePage";
+
+// Switch the page to render by commenting out either of the render() call
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +12,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <DifficultyTablePage />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
---
+++ view/src/pages/DifficultyTablePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./DifficultyTablePage.css";
+import { Row } from "../components/Row";
+import { stages } from "core";
+
+export const DifficultyTablePage: React.VFC = () => {
+  return <div className="DifficultyTablePage">Difficulty</div>;
+};
