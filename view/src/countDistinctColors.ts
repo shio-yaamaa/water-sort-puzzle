997b33a9b356e39eae31f63dec7f8ba726c5295b
shio-yaamaa <shio.yama520@gmail.com>
2021-03-31 03:09:18 +0900

    Exclude Water.EMPTY when counting colors

--- view/src/countDistinctColors.ts
+++ view/src/countDistinctColors.ts
@@ -1,6 +1,9 @@
-import { Stage } from "core";
+import { Stage, Water } from "core";
 
 export const countDistinctColors = (stage: Stage): number => {
-  const waters = stage.tubes.map((tubes) => tubes.waters).flat();
+  const waters = stage.tubes
+    .map((tubes) => tubes.waters)
+    .flat()
+    .filter((water) => water !== Water.EMPTY);
   return new Set(waters).size;
 };
